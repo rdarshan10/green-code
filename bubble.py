1401e039a7909538e7e9f3b58e9e4ca5ae263e9b
Sreyaaas <sm7183@srmist.edu.in>
2025-03-12 13:02:11 +0530

    Testing husky

--- bubble.py
+++ bubble.py
@@ -1,13 +1,10 @@
-def bubble_sort(arr):
-    n = len(arr)
-    for i in range(n):
-        for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
-                # Swap the elements
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr
+
+from typing import List
+
+def timsort(arr: List[int]) -> List[int]:
+    return sorted(arr)
 
 # Example usage
 arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = bubble_sort(arr)
+sorted_arr = timsort(arr)
 print("Sorted array:", sorted_arr)
