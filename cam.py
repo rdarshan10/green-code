69a1496c401d6625f542a8a45fa421b415fddb8b
Sreyaaas <sm7183@srmist.edu.in>
2025-03-18 16:26:33 +0530

    OP

--- cam.py
+++ cam.py
@@ -4,14 +4,10 @@ def timsort(arr):
 # Example usage:
 arr = [64, 34, 25, 12, 22, 11, 90]
 timsort(arr)
-    arr.sort()
-
-# Example usage:
+print("Sorted array:", arr)
 arr = [64, 34, 25, 12, 22, 11, 90]
 timsort(arr)
-    arr.sort()
-
-# Example usage:
+print("Sorted array:", arr)
 arr = [64, 34, 25, 12, 22, 11, 90]
 timsort(arr)
 print("Sorted array:", arr)
