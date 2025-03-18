c9e9cace417de06d4a0f680092ea56214ceeebb6
Sreyaaas <sm7183@srmist.edu.in>
2025-03-18 16:25:26 +0530

    OP

--- cam.py
+++ cam.py
@@ -1,7 +1,7 @@
 def timsort(arr):
-    return sorted(arr)
+    arr.sort()
 
 # Example usage:
 arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = timsort(arr)
-print("Sorted array:", sorted_arr)
+timsort(arr)
+print("Sorted array:", arr)
