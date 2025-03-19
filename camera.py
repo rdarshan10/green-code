bf8ecc66f015258bfe67872179730aafe0dd9d91
Sreyaaas <sm7183@srmist.edu.in>
2025-03-19 11:18:01 +0530

    Your commit message

--- camera.py
+++ camera.py
@@ -1,17 +1,13 @@
-def bubble_sort(arr):
-    n = len(arr)
-    n2 = len(arr)
+def bubbleSort(data):
+    n = len(data)
     for i in range(n):
         swapped = False
-        for j in range(n - i - 1):
-            if arr[j] > arr[j + 1]:
-                n2+=1
+        for j in range(0, n-i-1):
+            if data[j] > data[j+1]:
+                temp = data[j]
+                data[j] = data[j+1]
+                data[j+1] = temp
                 swapped = True
         if not swapped:
-            break  # Optimization: If no swaps, array is already sorted
-    return arr
-
-# Example usage
-arr = [64, 34, 25, 12, 22, 11, 90]
-sorted_arr = bubble_sort(arr)
-print("Sorted array:", sorted_arr)
+            break
+    return data
