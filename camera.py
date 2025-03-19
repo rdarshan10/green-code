30ff661f6cd838f3453c4f087ff31aba5f4f6cf1
Sreyaaas <sm7183@srmist.edu.in>
2025-03-19 11:14:18 +0530

    camera

--- camera.py
+++ camera.py
@@ -1,9 +1,11 @@
 def bubble_sort(arr):
     n = len(arr)
+    n2 = len(arr)
     for i in range(n):
         swapped = False
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
+                n2+=1
                 swapped = True
         if not swapped:
             break  # Optimization: If no swaps, array is already sorted
