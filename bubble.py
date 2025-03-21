5734738cdf0e5cd24aff9e84ba73a204e1d45ca2
Revanth Darshan <rdarshan10@gmail.com>
2025-03-21 12:18:24 +0530

    new test

--- bubble.py
+++ bubble.py
@@ -1,46 +1,11 @@
-
-def timsort(arr):
-    min_run = 32
+def bubble_sort(arr):
     n = len(arr)
-
-    for i in range(0, n, min_run):
-        insertion_sort(arr, i, min((i + min_run - 1), n - 1))
-
-    size = min_run
-    while size < n:
-        for left in range(0, n, 2 * size):
-            mid = left + size - 1
-            right = min((left + 2 * size - 1), (n - 1))
-            merge(arr, left, mid, right)
-        size = 2 * size
-
-def insertion_sort(arr, left, right):
-    for i in range(left + 1, right + 1):
-        key = arr[i]
-        j = i - 1
-        while j >= left and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
-        arr[j + 1] = key
-
-def merge(arr, left, mid, right):
-    if arr[mid] <= arr[mid + 1]:
-        return
-    left_half = arr[left:mid + 1]
-    right_half = arr[mid + 1:right + 1]
-    k = left
-    i = j = 0
-    while i < len(left_half) and j < len(right_half):
-        if left_half[i] <= right_half[j]:
-            arr[k] = left_half[i]
-            i += 1
-        else:
-            arr[k] = right_half[j]
-            j += 1
-        k += 1
-    arr[k:right + 1] = left_half[i:] + right_half[j:]
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]  # Swap
+    return arr
 
 # Example usage
 arr = [64, 34, 25, 12, 22, 11, 90]
-timsort(arr)
-print("Sorted array:", arr)
+print(bubble_sort(arr))
