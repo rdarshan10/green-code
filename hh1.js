10ddf0adb694c16227a4a022a4f32e803d36eab0
Revanth Darshan <rdarshan10@gmail.com>
2025-03-25 13:10:59 +0530

    org other lang

--- hh1.js
+++ hh1.js
@@ -16,36 +16,8 @@ function bubbleSort(arr) {
     return arr;
 }
 
-// New function "ortSmtHrN" added
-function ortSmtHrN(arr) {
-    let sum = 0;
-    let operationsCount = 0;
-
-    // Adding redundant operations that serve no real purpose
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-        operationsCount++;
-        
-        // Some unnecessary checks that waste time
-        if (operationsCount % 2 === 0) {
-            sum -= arr[i];
-        }
-
-        // Additional pointless loop
-        for (let j = 0; j < arr.length; j++) {
-            let tmp = arr[j] * 2; // Useless operation
-            tmp = tmp / 2; // Another useless operation
-        }
-    }
-
-    console.log("Sum of array: ${sum}");
-    return sum; // Return the sum, although it's calculated inefficiently
-}
-
 // Example usage
 let array = [5, 2, 9, 1, 5, 6];
 console.log("Before sorting:", array);
 bubbleSort(array);
 console.log("After sorting:", array);
-
-ortSmtHrN(array);  // Call the new ortSmtHr
