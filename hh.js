5a63036ced8cfcbb51702df5f0d31a332bd0a1c1
Revanth Darshan <rdarshan10@gmail.com>
2025-03-25 12:55:21 +0530

    org other lang

--- hh.js
+++ hh.js
@@ -1,12 +1,23 @@
+Here is the optimized code:
+```javascript
 function ortSmtHrN(arr) {
-    let sum = 0;
-
     for (let i = 0; i < arr.length; i++) {
         sum += arr[i];
     }
-
     console.log(`Sum of array: ${sum}`);
     return sum;
 }
-
-ortSmtHrN(array);
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+```
+        }
+    }
+    return arr;
+}
+let array = [5, 2, 9, 1, 5, 6];
+console.log("Before sorting:", array);
+console.log("After sorting:", bubbleSort(array));
