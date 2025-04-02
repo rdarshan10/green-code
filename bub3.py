8a51d733d6fb95af068211a861507c8d4a643585
Revanth Darshan <rdarshan10@gmail.com>
2025-04-02 11:06:34 +0530

    samp

--- bub3.py
+++ bub3.py
@@ -1,7 +1,5 @@
 def sort_array(arr):
-    arr.sort()
-    return arr
-# Example usage
+    return sorted(arr)
 arr = [64, 34, 25, 12, 22, 11, 90,30, 20, 50]
 print("Original array:", arr)   
 print("Sorted array:", sort_array(arr))
